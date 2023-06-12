f3bbd4d86eda51251a791c70443fc797772b0f34
= <=>
2023-06-12 15:27:01 -0500

    token

--- src/functions/GetToken.ts
+++ src/functions/GetToken.ts
@@ -1,15 +1,43 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from '@azure/functions';
 
-export async function GetToken(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+// import { ChatClient } from '@azure/communication-chat';
+// import { AzureCommunicationTokenCredential } from '@azure/communication-common';
+import { CommunicationIdentityClient } from '@azure/communication-identity';
 
-    const name = request.query.get('name') || await request.text() || 'world';
+// let endpointUrl = process.env['ChatEndpointUrl'];
+// let secretKey = process.env['ChatSecret'];
+let connectionString = process.env['ChatConnectionString'];
 
-    return { body: `Hello, ${name}!` };
-};
+// let chatClient = new ChatClient(
+//   endpointUrl,
+//   new AzureCommunicationTokenCredential(secretKey)
+// );
+
+console.log('Azure Communication Chat client created!');
+export async function GetToken(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+
+  context.log(connectionString);
+
+  const name = request.query.get('name') || (await request.text()) || 'world';
+
+  const identityClient = new CommunicationIdentityClient(connectionString);
+  const user = await identityClient.createUser();
+
+  // const token = await identityClient.getToken(user, ['chat']);
+  return { body: `Hello, ${name}!` };
+}
 
 app.http('GetToken', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: GetToken
+  methods: ['GET'],
+  authLevel: 'anonymous',
+  handler: GetToken,
 });
